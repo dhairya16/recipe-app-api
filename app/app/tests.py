aa95a724569e3538f26af8ccfe8fd2988b389759
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2023-04-15 10:43:51 +0530

    Database configured

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTest(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_number(self):
+        res = calc.add_number(1, 2)
+        self.assertEqual(3, res)
+
+
